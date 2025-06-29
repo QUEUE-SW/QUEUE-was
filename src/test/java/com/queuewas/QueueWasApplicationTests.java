6699cab5a354c0fd366e8d7b8a47015d6bbe7b2b
mingking2 <mingee641@gmail.com>
2025-06-29 22:24:32 +0900

    chore: testcontainer 추가 및 적용

--- src/test/java/com/queuewas/QueueWasApplicationTests.java
+++ src/test/java/com/queuewas/QueueWasApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QueueWasApplicationTests {
+class QueueWasApplicationTests extends TestDatabaseConfig{
 
 	@Test
 	void contextLoads() {
