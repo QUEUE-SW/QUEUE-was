9029814a6d1db633c42246f5bbf7926b48898c51
King's meow <mingee641@gmail.com>
2025-06-29 22:26:40 +0900

    [QUEUE-190] Github 자동화 파이프라인 구축 (#2)
    * chore: ci 빌드 자동화 추가
    
    * chore: 도커 파일 추가
    
    * chore: cd 배포 파이프라인 구축
    
    * chore: testcontainer 추가 및 적용
    
    * close #QUEUE-190

---
+++ src/test/java/com/queuewas/TestDatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.queuewas;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class TestDatabaseConfig {
+
+	private static final String MYSQL_CONTAINER_IMAGE = "mysql:8.0.35";
+	private static final MySQLContainer MYSQL_CONTAINER;
+
+	static {
+		MYSQL_CONTAINER = new MySQLContainer(DockerImageName.parse(MYSQL_CONTAINER_IMAGE));
+		MYSQL_CONTAINER.start();
+	}
+
+	@DynamicPropertySource
+	public static void overrideProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+		registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+		registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+	}
+}
