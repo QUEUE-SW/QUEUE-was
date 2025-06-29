1ccb21867e6df41397cc12fc5b003563340f526e
mingking2 <mingee641@gmail.com>
2025-06-29 10:52:46 +0900

    init commit

---
+++ src/main/java/com/queuewas/QueueWasApplication.java
@@ -0,0 +1,13 @@
+package com.queuewas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queuewas/QueueWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queuewas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
