a6d916a7446d42298db0b1a46b0deda4e86ed7e2
mingking2 <mingee641@gmail.com>
2025-07-03 23:27:20 +0900

    chore: 대기열 에러코드 추가

--- src/main/java/com/queuewas/common/exception/queue/QueueErrorCode.java
+++ src/main/java/com/queuewas/common/exception/queue/QueueErrorCode.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum QueueErrorCode implements BaseErrorCode {
 
-	FAILED_JOIN_QUEUE(StatusCode.BAD_REQUEST, ReasonCode.INVALID_REQUEST, "대기열 진입에 실패했습니다.");
+	FAILED_JOIN_QUEUE(StatusCode.BAD_REQUEST, ReasonCode.INVALID_REQUEST, "대기열 진입에 실패했습니다."),
+	QUEUE_NOT_FOUND(StatusCode.NOT_FOUND, ReasonCode.REQUESTED_RESOURCE_NOT_FOUND, "해당하는 대기열 정보가 없습니다.");
 
 	private final StatusCode statusCode;
 	private final ReasonCode reasonCode;
