e20aafc53b732fe1b465bb2be026e0c3d8d5a2d6
mingking2 <mingee641@gmail.com>
2025-07-03 12:23:15 +0900

    refactor: 불필요한 계층 분리 제거 및 적용

--- src/main/java/com/queuewas/domains/queue/service/QueueService.java
+++ src/main/java/com/queuewas/domains/queue/service/QueueService.java
@@ -1,26 +1,27 @@
 package com.queuewas.domains.queue.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.queuewas.domains.queue.QueueConsumer;
-import com.queuewas.domains.queue.QueueProducer;
 import com.queuewas.domains.queue.domain.QueueUser;
 import com.queuewas.domains.queue.dto.request.QueueJoinReq;
 import com.queuewas.domains.queue.dto.response.QueueJoinRes;
+import com.queuewas.domains.queue.implement.QueueManager;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class QueueService {
-
-	private final QueueProducer queueProducer;
-	private final QueueConsumer queueConsumer;
+	private final QueueManager queueManager;
 
 	public QueueJoinRes join(QueueJoinReq queueJoinReq) {
-		QueueUser user = queueProducer.submitUser(queueJoinReq.token());
+		String token = queueJoinReq.token();
+		long joinedAt = System.currentTimeMillis();
+		int queueNumber = queueManager.getQueueNumber(token);
+
+		QueueUser user = QueueUser.create(token, joinedAt, queueNumber);
+		queueManager.enqueue(user);
+
 		return QueueJoinRes.from(user);
 	}
 }
