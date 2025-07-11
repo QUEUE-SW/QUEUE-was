c25ade6b596fcf44550f9225f42abb34b5139eb5
MinjiSeo16 <mjlove6630@naver.com>
2025-07-12 01:40:28 +0900

    feat: SseQueueService에 입장 대상자 SSE 알림 전송 로직 구현

--- src/main/java/com/queuewas/domains/queue/service/SseQueueService.java
+++ src/main/java/com/queuewas/domains/queue/service/SseQueueService.java
@@ -1,8 +1,13 @@
 package com.queuewas.domains.queue.service;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.queuewas.domains.queue.domain.QueueUser;
+import com.queuewas.domains.queue.implement.QueueManager;
 import com.queuewas.domains.queue.implement.SseEmitterManager;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SseQueueService {
 
 	private final SseEmitterManager emitterManager;
+	private final QueueManager queueManager;
 
 	public SseEmitter subscribe(String token) {
 		SseEmitter emitter = new SseEmitter(60_000L);
@@ -24,5 +30,26 @@ public class SseQueueService {
 		emitter.onError((e) -> emitterManager.removeEmitter(token));
 
 		return emitter;
+	}
+
+	public void notifyEntranceToUsers(int count) {
+		List<QueueUser> users = queueManager.popUsers(count);
+		if (users.isEmpty()) {
+			return;
+		}
+
+		for (QueueUser user : users) {
+			String token = user.getToken();
+			SseEmitter emitter = emitterManager.getEmitter(token);
+			if (emitter != null) {
+				try {
+					emitter.send(SseEmitter.event()
+						.name("entrance")
+						.data("입장 가능합니다."));
+				} catch (IOException e) {
+					emitterManager.removeEmitter(token);
+				}
+			}
+		}
 	}
 }
