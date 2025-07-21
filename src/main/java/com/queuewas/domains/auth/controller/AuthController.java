7f778932613e9d93649457b48b56882260863714
MinjiSeo16 <mjlove6630@naver.com>
2025-07-21 12:52:39 +0900

    refactor: SseQueueService 적용

--- src/main/java/com/queuewas/domains/auth/controller/AuthController.java
+++ src/main/java/com/queuewas/domains/auth/controller/AuthController.java
@@ -12,7 +12,7 @@ import com.queuewas.common.response.SuccessResponse;
 import com.queuewas.domains.auth.dto.request.SignInReq;
 import com.queuewas.domains.auth.dto.response.SignInRes;
 import com.queuewas.domains.auth.service.AuthService;
-import com.queuewas.domains.queue.service.PollingQueueService;
+import com.queuewas.domains.queue.service.SseQueueService;
 import com.queuewas.domains.queue.type.QueueStatus;
 
 import jakarta.validation.Valid;
@@ -26,11 +26,12 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthController {
 
 	private final AuthService authService;
-	private final PollingQueueService queueService;
+	private final SseQueueService queueService;
 
 	@PostMapping("/sign-in")
 	public ResponseEntity<?> signIn(@Valid @RequestBody SignInReq signInReq) {
-		log.info("[SignIn] identifier: {} password: {} uuid: {}", signInReq.identifier(), signInReq.password(), signInReq.uuid());
+		log.info("[SignIn] identifier: {} password: {} uuid: {}", signInReq.identifier(), signInReq.password(),
+			signInReq.uuid());
 		QueueStatus queueStatus = queueService.readStatus(signInReq.uuid()).queueStatus();
 		if (!queueStatus.equals(QueueStatus.ALLOWED)) {
 			throw new QueueException(QueueErrorCode.QUEUE_IS_NOT_ALLOWED);
