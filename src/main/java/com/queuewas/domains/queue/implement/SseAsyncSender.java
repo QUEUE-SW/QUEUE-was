6ab34d364079ca0f413b177fdd64c7331ff28b7c
MinjiSeo16 <mjlove6630@naver.com>
2025-07-25 16:51:52 +0900

    refactor: SseAsyncSender의 send()에 allowed 상태일때만 complete 되도록 if문 추가

--- src/main/java/com/queuewas/domains/queue/implement/SseAsyncSender.java
+++ src/main/java/com/queuewas/domains/queue/implement/SseAsyncSender.java
@@ -22,10 +22,13 @@ public class SseAsyncSender {
 			emitter.send(SseEmitter.event()
 				.name(event)
 				.data(data));
-			emitter.complete();
 			log.info("✅ send 성공!!");
-			emitterManager.removeEmitter(token);
-			log.info("✅ emitter 종료 및 제거 완료: token={}", token);
+
+			if ("allowed".equals(event)) {
+				emitter.complete();
+				emitterManager.removeEmitter(token);
+				log.info("✅ emitter 종료 및 제거 완료: token={}", token);
+			}
 		} catch (IOException e) {
 			log.warn("🚨 SSE 전송 실패: event={}, message={}", event, e.getMessage());
 		}
