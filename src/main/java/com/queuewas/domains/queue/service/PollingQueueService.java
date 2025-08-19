4719ce80c874dbfbc6b1d378bc56152207df019f
MinjiSeo16 <mjlove6630@naver.com>
2025-08-19 16:38:01 +0900

    refactor: polling 기반으로 QueueController 구조 정리

--- src/main/java/com/queuewas/domains/queue/service/PollingQueueService.java
+++ src/main/java/com/queuewas/domains/queue/service/PollingQueueService.java
@@ -1,23 +1,13 @@
-/*
 package com.queuewas.domains.queue.service;
 
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import com.queuewas.common.exception.queue.QueueErrorCode;
-import com.queuewas.common.exception.queue.QueueException;
-import com.queuewas.domains.queue.domain.QueueUser;
 import com.queuewas.domains.queue.dto.request.QueueJoinReq;
 import com.queuewas.domains.queue.dto.response.QueueJoinRes;
 import com.queuewas.domains.queue.dto.response.QueueStatusRes;
-import com.queuewas.domains.queue.implement.BatchManager;
-import com.queuewas.domains.queue.implement.QueueManager;
+import com.queuewas.domains.queue.implement.RedisQueueManager;
 import com.queuewas.domains.queue.type.QueueStatus;
 
 import lombok.RequiredArgsConstructor;
@@ -27,74 +17,36 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class PollingQueueService {
-	private final QueueManager queueManager;
-	private final BatchManager batchManager;
-	private final ScheduledExecutorService scheduler;
-	private final RestTemplate restTemplate;
+
+	private final RedisQueueManager redisQueueManager;
 
 	public QueueJoinRes join(QueueJoinReq queueJoinReq) {
 		String token = queueJoinReq.token();
-		queueManager.enqueue(token);
 
-		long queueNumber = queueManager.getQueueNumber(token);
+		if (!redisQueueManager.isAllowed(token)) {
+			redisQueueManager.enqueue(token);
+		}
 
+		long queueNumber = redisQueueManager.getQueueNumber(token);
 		return QueueJoinRes.from(queueNumber);
 	}
 
 	public QueueStatusRes readStatus(String token) {
-		QueueUser queueUser = queueManager.getQueueUser(token);
-		if (queueUser == null) {
-			throw new QueueException(QueueErrorCode.QUEUE_NOT_FOUND);
-		}
-
-		long queueNumber = queueManager.getQueueNumber(token);
-
-		return QueueStatusRes.from(queueNumber, queueUser.getStatus());
+		long queueNumber = redisQueueManager.getQueueNumber(token);
+		return (queueNumber == 0L) ? QueueStatusRes.from(0L, QueueStatus.ALLOWED) :
+			QueueStatusRes.from(queueNumber, QueueStatus.WAITING);
 	}
 
-	public CompletableFuture<Void> allowQueueStatusWithAck(int count) {
-		List<QueueUser> users = queueManager.popUsers(count);
-		if (users.isEmpty()) {
-			return CompletableFuture.completedFuture(null);
+	public void notifyEntranceToUsers(int count) {
+		List<String> tokens = redisQueueManager.popTokens(count);
+		if (tokens == null || tokens.isEmpty()) {
+			return;
 		}
-
-		String batchId = batchManager.registerBatch(users);
-		CompletableFuture<Void> future = batchManager.getFuture(batchId);
-
-		scheduler.schedule(() -> batchManager.completeBatchPartially(batchId), 10, TimeUnit.SECONDS);
-
-		return future;
+		redisQueueManager.markAllowed(tokens);
 	}
 
 	public void notifyLogin(String token) {
-		QueueUser user = queueManager.getQueueUser(token);
-		batchManager.notifyUserLogin(token, user);
-		queueManager.remove(token);
-	}
-
-	public void removeQueueInfo(String token) {
-		queueManager.remove(token);
-	}
-
-	public void rollbackBatch(String batchId) {
-		Set<String> tokens = batchManager.getTokens(batchId);
-
-		if (tokens == null || tokens.isEmpty())
-			return;
-
-		for (String token : tokens) {
-			QueueUser user = queueManager.getQueueUser(token);
-			if (user != null && user.getStatus() == QueueStatus.ALLOWED) {
-				user.updateStatus(QueueStatus.WAITING);
-				queueManager.requeue(user);
-			}
-		}
-
-		batchManager.removeBatch(batchId);
-	}
-
-	public void reset() {
-		queueManager.reset();
+		redisQueueManager.unmarkAllowed(token);
+		redisQueueManager.remove(token);
 	}
 }
-*/
