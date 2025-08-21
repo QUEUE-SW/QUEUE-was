83856236ce0d4e9545fb8edce6cad11e4d7c83ae
MinjiSeo16 <mjlove6630@naver.com>
2025-08-21 22:41:46 +0900

    refactor: polling 엔드포인트를 기존 경로로 변경

--- src/main/java/com/queuewas/domains/queue/controller/PollingQueueController.java
+++ src/main/java/com/queuewas/domains/queue/controller/PollingQueueController.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/queue/polling")
+@RequestMapping("/api/v1/queue")
 @RequiredArgsConstructor
 public class PollingQueueController {
 
